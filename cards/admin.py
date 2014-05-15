e8fa31753813ddba4b678524a94bd83790710624
thisisthechris <mail@thisisthechris.co.uk>
2014-05-15 10:10:47 +0100

    Adds Admin Filtering to Scans

--- cards/admin.py
+++ cards/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from models import *
+from core.actions import export_as_xls
+
+class ScanAdmin(admin.ModelAdmin):
+    list_filter = ['readerLocation', 'added']
 
 # Register your models here.
 admin.site.register(Batch)
@@ -8,4 +12,9 @@ admin.site.register(Card)
 admin.site.register(Reader)
 admin.site.register(Location)
 admin.site.register(ReaderLocation)
-admin.site.register(Scan)
+admin.site.register(Scan, ScanAdmin)
+
+class MyAdmin(admin.ModelAdmin):
+    actions = [export_as_xls]
+
+admin.site.add_action(export_as_xls)
